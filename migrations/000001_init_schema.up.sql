1a1db71253b27e9fd29b89799a7260182d5b85d5
daffadon <daffaputranarendra9@gmail.com>
2026-04-02 21:19:18 +0800

    refactor(migration): change ERD

--- migrations/000001_init_schema.up.sql
+++ migrations/000001_init_schema.up.sql
@@ -6,22 +6,22 @@ CREATE TABLE IF NOT EXISTS project(
   updated_at TIMESTAMPTZ
 );
 
-CREATE TABLE IF NOT EXISTS container(
-	id UUID PRIMARY KEY DEFAULT uuidv7(),
-  name VARCHAR NOT NULL,
-  port_exposed integer[],
-  created_at TIMESTAMPTZ DEFAULT now(),
-  updated_at TIMESTAMPTZ
-);
-  
 CREATE TABLE IF NOT EXISTS deployment(
 	id UUID PRIMARY KEY DEFAULT uuidv7(),
   name VARCHAR NOT NULL,
   s3_remote_url VARCHAR,
   version varchar,
-  project_id UUID NOT NULL REFERENCES project(id),
-  container_id UUID NOT NULL REFERENCES container(id),
+  project_id UUID NOT NULL REFERENCES project(id) ON DELETE CASCADE,
   created_at TIMESTAMPTZ DEFAULT now(),
   updated_at TIMESTAMPTZ,
   CONSTRAINT deployment_project_name_unique UNIQUE (project_id, name)
+);
+
+CREATE TABLE IF NOT EXISTS container(
+	id UUID PRIMARY KEY DEFAULT uuidv7(),
+  name VARCHAR NOT NULL,
+  port_exposed integer[],
+  deployment_id UUID NOT NULL UNIQUE REFERENCES deployment(id) ON DELETE CASCADE,
+  created_at TIMESTAMPTZ DEFAULT now(),
+  updated_at TIMESTAMPTZ
 );
