4ff75c61df4c37b60e55042be075ff0ef3d81a0c
daffadon <daffaputranarendra9@gmail.com>
2026-04-02 21:18:44 +0800

    feat(project): add handler and services for project

--- internal/domain/handler/project.go
+++ internal/domain/handler/project.go
@@ -3,12 +3,18 @@ package handler
 import (
 	"net/http"
 
+	"github.com/server-selfish/backend/internal/domain/schema"
 	"github.com/server-selfish/backend/internal/domain/service"
+	"github.com/server-selfish/backend/internal/pkg"
 )
 
 type (
 	ProjectHandler interface {
 		CreateProject(w http.ResponseWriter, r *http.Request)
+		GetAllProjects(w http.ResponseWriter, r *http.Request)
+		GetProjectById(w http.ResponseWriter, r *http.Request)
+		UpdateProjectById(w http.ResponseWriter, r *http.Request)
+		DeleteProjectById(w http.ResponseWriter, r *http.Request)
 	}
 	projectHandler struct {
 		ps service.ProjectService
@@ -21,6 +27,85 @@ func NewProjectHandler(ps service.ProjectService) ProjectHandler {
 	}
 }
 
+// DeleteProjectById implements [ProjectHandler].
+func (p *projectHandler) DeleteProjectById(w http.ResponseWriter, r *http.Request) {
+	req, ok := pkg.DecodeAndValidate[schema.DeleteProjectParams](w, r)
+	if !ok {
+		return
+	}
+	ctx := r.Context()
+	id, err := pkg.StringToPgUUID(req.ID)
+	if err != nil {
+		pkg.ReturnError(w, pkg.ErrBadRequest)
+		return
+	}
+	if err := p.ps.DeleteProjectById(ctx, id); err != nil {
+		pkg.ReturnError(w, err)
+		return
+	}
+	pkg.ReturnSuccess(w, http.StatusOK, "project deleted", nil)
+}
+
+// GetAllProjects implements [ProjectHandler].
+func (p *projectHandler) GetAllProjects(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	projects, err := p.ps.GetAllProjects(ctx)
+	if err != nil {
+		pkg.ReturnError(w, err)
+		return
+	}
+	pkg.ReturnSuccess(w, http.StatusOK, "fetch success", projects)
+}
+
+// GetProjectById implements [ProjectHandler].
+func (p *projectHandler) GetProjectById(w http.ResponseWriter, r *http.Request) {
+	req, ok := pkg.DecodeAndValidate[schema.GetProjectParams](w, r)
+	if !ok {
+		return
+	}
+	ctx := r.Context()
+	id, err := pkg.StringToPgUUID(req.ID)
+	if err != nil {
+		pkg.ReturnError(w, pkg.ErrBadRequest)
+		return
+	}
+	project, err := p.ps.GetProjectById(ctx, id)
+	if err != nil {
+		pkg.ReturnError(w, err)
+		return
+	}
+	pkg.ReturnSuccess(w, http.StatusCreated, "fetch success", project)
+}
+
+// UpdateProjectById implements [ProjectHandler].
+func (p *projectHandler) UpdateProjectById(w http.ResponseWriter, r *http.Request) {
+	req, ok := pkg.DecodeAndValidate[schema.UpdateProjectParams](w, r)
+	if !ok {
+		return
+	}
+	ctx := r.Context()
+	_, err := pkg.StringToPgUUID(req.ID)
+	if err != nil {
+		pkg.ReturnError(w, pkg.ErrBadRequest)
+		return
+	}
+	if err := p.ps.UpdateProjectById(ctx, &req); err != nil {
+		pkg.ReturnError(w, err)
+		return
+	}
+	pkg.ReturnSuccess(w, http.StatusOK, "project updated", nil)
+}
+
+// CreateProject implements [ProjectHandler].
 func (p *projectHandler) CreateProject(w http.ResponseWriter, r *http.Request) {
-	panic("unimplemented")
+	req, ok := pkg.DecodeAndValidate[schema.CreateProjectParams](w, r)
+	if !ok {
+		return
+	}
+	ctx := r.Context()
+	if err := p.ps.CreateProject(ctx, &req); err != nil {
+		pkg.ReturnError(w, err)
+		return
+	}
+	pkg.ReturnSuccess(w, http.StatusCreated, "Project Created", nil)
 }
--- internal/domain/service/project.go
+++ internal/domain/service/project.go
@@ -2,35 +2,107 @@ package service
 
 import (
 	"context"
+	"errors"
 
-	"github.com/server-selfish/backend/internal/domain/dto"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgtype"
 	project_repository "github.com/server-selfish/backend/internal/domain/repository/project"
+	"github.com/server-selfish/backend/internal/domain/schema"
+	"github.com/server-selfish/backend/internal/pkg"
 )
 
 type (
 	ProjectService interface {
-		CreateProject(ctx context.Context, p *dto.Project) bool
+		CreateProject(ctx context.Context, params *schema.CreateProjectParams) error
+		GetAllProjects(ctx context.Context) ([]schema.GetProjectsData, error)
+		GetProjectById(ctx context.Context, id pgtype.UUID) (schema.GetProjectsData, error)
+		UpdateProjectById(ctx context.Context, params *schema.UpdateProjectParams) error
+		DeleteProjectById(ctx context.Context, id pgtype.UUID) error
 	}
 	projectService struct {
 		pr *project_repository.Queries
+		tm pkg.TxManager
 	}
 )
 
-func NewProjectService(pr *project_repository.Queries) ProjectService {
+func NewProjectService(pr *project_repository.Queries, tm pkg.TxManager) ProjectService {
 	return &projectService{
 		pr: pr,
+		tm: tm,
 	}
 }
 
-func (ps *projectService) CreateProject(ctx context.Context, p *dto.Project) bool {
-	// if err := ps.txRunner.RunInTx(ctx, func(q repository.Querier) error {
-	// 	repo := ps.prf.Create(q)
-	// 	return repo.CreateProject(p)
-	// }); err != nil {
-	// 	return false
-	// }
-	// return true
-	// repo := ps.prf.Default()
-	// return repo.CreateProject(p)
-	panic("undimplemented")
+// DeleteProjectById implements [ProjectService].
+func (ps *projectService) DeleteProjectById(ctx context.Context, id pgtype.UUID) error {
+	return ps.pr.DeleteProjectById(ctx, id)
+}
+
+// GetAllProjects implements [ProjectService].
+func (ps *projectService) GetAllProjects(ctx context.Context) ([]schema.GetProjectsData, error) {
+	projects, err := ps.pr.GetAllProjects(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if len(projects) == 0 {
+		return nil, pkg.ErrNotFound
+	}
+	var res []schema.GetProjectsData
+	for _, p := range projects {
+		res = append(res, schema.GetProjectsData{
+			ID:          p.ID.String(),
+			Name:        p.Name,
+			Description: p.Description.String,
+		})
+	}
+	return res, nil
+}
+
+// GetProjectById implements [ProjectService].
+func (ps *projectService) GetProjectById(ctx context.Context, id pgtype.UUID) (schema.GetProjectsData, error) {
+	p, err := ps.pr.GetProjectById(ctx, id)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return schema.GetProjectsData{}, pkg.ErrNotFound
+		}
+		return schema.GetProjectsData{}, err
+	}
+	res := schema.GetProjectsData{
+		ID:          p.ID.String(),
+		Name:        p.Name,
+		Description: p.Description.String,
+	}
+	return res, nil
+}
+
+// UpdateProjectById implements [ProjectService].
+func (ps *projectService) UpdateProjectById(ctx context.Context, params *schema.UpdateProjectParams) error {
+	id, _ := pkg.StringToPgUUID(params.ID)
+	if err := ps.pr.UpdateProjectById(ctx, project_repository.UpdateProjectByIdParams{
+		Name:        params.Name,
+		Description: pgtype.Text{String: params.Description},
+		ID:          id,
+	}); err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+			return pkg.ErrAlreadyExist
+		}
+		return err
+	}
+	return nil
+}
+
+// UpdateProjectById implements [ProjectService].
+func (ps *projectService) CreateProject(ctx context.Context, params *schema.CreateProjectParams) error {
+	if err := ps.pr.CreateProject(ctx, project_repository.CreateProjectParams{
+		Name:        params.Name,
+		Description: pgtype.Text{String: params.Description},
+	}); err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+			return pkg.ErrAlreadyExist
+		}
+		return err
+	}
+	return nil
 }
