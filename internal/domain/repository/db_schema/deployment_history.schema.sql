c2fd703dc8f2340cf3fadd9f424a1aa5a874e709
daffadon <daffaputranarendra9@gmail.com>
2026-04-16 09:42:17 +0800

    refactor(db_schema): update table for deployment_history

--- internal/domain/repository/db_schema/deployment_history.schema.sql
+++ internal/domain/repository/db_schema/deployment_history.schema.sql
@@ -1,7 +1,11 @@
 CREATE TABLE IF NOT EXISTS deployment_techstack(
   id SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL,
-  version VARCHAR NOT NULL
+  version VARCHAR NOT NULL,
+  docker_base_image VARCHAR NOT NULL,
+  docker_runtime_image VARCHAR NOT NULL,
+  created_at TIMESTAMPTZ default now(),
+  updated_at TIMESTAMPTZ
 );
 
 CREATE TABLE IF NOT EXISTS deployment_history(
@@ -9,11 +13,15 @@ CREATE TABLE IF NOT EXISTS deployment_history(
   deployment_id UUID NOT NULL REFERENCES deployment(id) ON DELETE CASCADE,
   git_remote_url VARCHAR NOT NULL,
   branch VARCHAR NOT NULL,
+  commit_id VARCHAR NOT NULL,
   commit_msg TEXT NOT NULL,
   version VARCHAR NOT NULL,
   external_port integer[],
   deployment_techstack_id INTEGER NOT NULL REFERENCES deployment_techstack(id) ON DELETE CASCADE,
   build_command VARCHAR,
+  build_folder VARCHAR,
+  run_command VARCHAR,
   is_active BOOL NOT NULL DEFAULT true,
-  created_at TIMESTAMPTZ default now()
+  created_at TIMESTAMPTZ default now(),
+  updated_at TIMESTAMPTZ
 );
