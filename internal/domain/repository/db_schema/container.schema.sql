ec955d0c25f8849969b55f9b900bbaa0972e7a15
daffadon <daffaputranarendra9@gmail.com>
2026-04-16 09:40:34 +0800

    feat(db_schema): change column reference and add env for container

--- internal/domain/repository/db_schema/container.schema.sql
+++ internal/domain/repository/db_schema/container.schema.sql
@@ -1,7 +1,15 @@
 CREATE TABLE IF NOT EXISTS container(
 	id UUID PRIMARY KEY DEFAULT uuidv7(),
   name VARCHAR NOT NULL,
-  deployment_id UUID NOT NULL UNIQUE REFERENCES deployment(id) ON DELETE CASCADE,
+  deployment_history_id INTEGER NOT NULL UNIQUE REFERENCES deployment_history(id) ON DELETE CASCADE,
   created_at TIMESTAMPTZ DEFAULT now(),
+  updated_at TIMESTAMPTZ
+);
+
+CREATE TABLET IF NOT EXISTS container_env(
+  id SERIAL PRIMARY KEY,
+  key VARCHAR NOT NULL,
+  value VARCHAR NOT NULL,
+  created_at TIMESTAMPTZ default now(),
   updated_at TIMESTAMPTZ
 );
