bc35f12d84f205375532a57259402366432a75c8
daffadon <daffaputranarendra9@gmail.com>
2026-04-16 10:14:34 +0800

    chore(migration): update table for dpeloyment_history

--- migrations/000001_init_schema.up.sql
+++ migrations/000001_init_schema.up.sql
@@ -14,11 +14,3 @@ CREATE TABLE IF NOT EXISTS deployment(
   updated_at TIMESTAMPTZ,
   CONSTRAINT deployment_project_name_unique UNIQUE (project_id, name)
 );
-
-CREATE TABLE IF NOT EXISTS container(
-	id UUID PRIMARY KEY DEFAULT uuidv7(),
-  name VARCHAR NOT NULL,
-  deployment_id UUID NOT NULL UNIQUE REFERENCES deployment(id) ON DELETE CASCADE,
-  created_at TIMESTAMPTZ DEFAULT now(),
-  updated_at TIMESTAMPTZ
-);
--- migrations/000002_deployment_history.up.sql
+++ migrations/000002_deployment_history.up.sql
@@ -1,7 +1,11 @@
 CREATE TABLE IF NOT EXISTS deployment_techstack(
   id SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL,
-  version VARCHAR NOT NULL
+  version VARCHAR NOT NULL,
+  docker_base_image VARCHAR NOT NULL,
+  docker_runtime_image VARCHAR NOT NULL,
+  created_at TIMESTAMPTZ default now(),
+  updated_at TIMESTAMPTZ
 );
 
 CREATE TABLE IF NOT EXISTS deployment_history(
@@ -9,10 +13,23 @@ CREATE TABLE IF NOT EXISTS deployment_history(
   deployment_id UUID NOT NULL REFERENCES deployment(id) ON DELETE CASCADE,
   git_remote_url VARCHAR NOT NULL,
   branch VARCHAR NOT NULL,
+  commit_id VARCHAR NOT NULL,
   commit_msg TEXT NOT NULL,
   version VARCHAR NOT NULL,
   external_port integer[],
-  deployment_techstack INTEGER NOT NULL REFERENCES deployment_techstack(id) ON DELETE CASCADE,
+  deployment_techstack_id INTEGER NOT NULL REFERENCES deployment_techstack(id) ON DELETE CASCADE,
   build_command VARCHAR,
+  build_folder VARCHAR,
+  run_command VARCHAR,
+  is_active BOOL NOT NULL DEFAULT true,
   created_at TIMESTAMPTZ default now(),
+  updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE IF NOT EXISTS container(
+	id UUID PRIMARY KEY DEFAULT uuidv7(),
+  name VARCHAR NOT NULL,
+  deployment_history_id INTEGER NOT NULL UNIQUE REFERENCES deployment_history(id) ON DELETE CASCADE,
+  created_at TIMESTAMPTZ DEFAULT now(),
+  updated_at TIMESTAMPTZ
 );
