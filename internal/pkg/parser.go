bbc297493390b8890dc1c5a7fc1f82aeb1f5cfd4
daffadon <daffaputranarendra9@gmail.com>
2026-04-21 11:48:20 +0800

    feat(utils): add RSA Private key parser from PEM

--- internal/pkg/parser.go
+++ internal/pkg/parser.go
@@ -2,6 +2,10 @@ package pkg
 
 import (
 	"bytes"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
 	"html/template"
 
 	docker_infra "github.com/server-selfish/backend/internal/infra/docker"
@@ -28,4 +32,24 @@ func ParseTemplateFromEmbed(name string, data interface{}) (string, error) {
 	}
 
 	return ParseTemplate(string(content), data)
+}
+
+func parseRSAPrivateKeyFromPEM(key string) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode([]byte(key))
+	if block == nil {
+		return nil, errors.New("invalid private key pem")
+	}
+
+	if pk, err := x509.ParsePKCS1PrivateKey(block.Bytes); err == nil {
+		return pk, nil
+	}
+
+	privAny, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err == nil {
+		if pk, ok := privAny.(*rsa.PrivateKey); ok {
+			return pk, nil
+		}
+	}
+
+	return nil, errors.New("failed to parse rsa private key")
 }
