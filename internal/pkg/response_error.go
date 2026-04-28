181884d0bcc16f134b805a527476cae4c67199bd
daffadon <daffaputranarendra9@gmail.com>
2026-04-28 18:07:45 +0800

    refactor(pkg): change error message

--- internal/pkg/response_error.go
+++ internal/pkg/response_error.go
@@ -5,5 +5,5 @@ import "errors"
 var (
 	ErrBadRequest   = errors.New("invalid request body")
 	ErrAlreadyExist = errors.New("resource is already exist")
-	ErrNotFound     = errors.New("resource already exist")
+	ErrNotFound     = errors.New("resource not found")
 )
